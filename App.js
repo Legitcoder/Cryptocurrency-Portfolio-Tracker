c5147f0d58c1fd77fb15707befca5c4f654221d5
Moin Uddin <moinuddin@Moins-MacBook-Pro.local>
2018-05-10 00:35:05 -0400

    Redux Setup

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import store from './store';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+     <Provider store={store}> 
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+        </View>
+      </Provider>
     );
   }
 }
