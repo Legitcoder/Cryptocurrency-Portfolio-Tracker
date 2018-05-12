7b8480fb26f029680e500e0dca8e7361e21d2af9
Moin Uddin <U_Moin75@yahoo.com>
2018-05-11 23:36:01 -0400

    Testing graphql

--- App.js
+++ App.js
@@ -2,15 +2,23 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './store';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import query from './queries';
+import { client } from './queries';
+
+
 
 export default class App extends React.Component {
   render() {
     return (
-     <Provider store={store}> 
-        <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-        </View>
-      </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store}> 
+          <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+          </View>
+        </Provider>
+      </ApolloProvider>
     );
   }
 }
