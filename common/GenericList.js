adf918b5a3cc932f3844b6b2722ec338ccef552d
Moin Uddin <U_Moin75@yahoo.com>
2018-05-25 06:52:55 -0400

    Begin creating reusable generic list for selecting exchanges and trading pairs for trasaction form

---
+++ common/GenericList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+
+
+class GenericList extends Component {
+
+    render() {
+        return(
+            <View />
+        );
+    }
+}
+
+
+const styles = StyleSheet.create({
+});
+
+
+
+export default GenericList;
